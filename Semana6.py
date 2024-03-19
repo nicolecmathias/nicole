0c5458fa60c658c2dff629be077196214e938781
nicolecmathias <159146482+nicolecmathias@users.noreply.github.com>
2024-03-19 10:46:41 -0300

    Add files via upload

---
+++ Semana6.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# Declaração das variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Imprime os valores das variáveis
+print("Número 1:", numero1)
+print("Número 2:", numero2)
+print("Número 3:", numero3)
+print("Número 4:", numero4)
+# Declaração das variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Calcula a média aritmética
+media = (numero1 + numero2 + numero3 + numero4) / 4
+
+# Imprime a média aritmética
+print("A média aritmética dos números é:", media)
+# Variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Escolha qual número calcular o quadrado (por exemplo, numero1)
+numero_escolhido = numero1
+
+# Calcula o quadrado do número escolhido
+quadrado = numero_escolhido ** 2
+
+# Imprime o quadrado do número escolhido
+print("O quadrado de", numero_escolhido, "é", quadrado)
+# Declaração das variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Escolha qual número calcular o dobro (por exemplo, numero1)
+numero_escolhido = numero1
+
+# Calcula o dobro do número escolhido
+dobro = numero_escolhido * 2
+
+# Imprime o dobro do número escolhido
+print("O dobro de", numero_escolhido, "é", dobro)
+# Declaração das variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Verifica se o primeiro número é maior que o segundo
+if numero1 > numero2:
+    print("O primeiro número é maior que o segundo.")
+else:
+    print("O primeiro número não é maior que o segundo.")
+# Declaração das variáveis para armazenar 4 números
+numero1 = 10
+numero2 = 20
+numero3 = 30
+numero4 = 40
+
+# Encontra o maior número
+maior_numero = max(numero1, numero2, numero3, numero4)
+
+# Imprime o maior número
+print("O maior número é:", maior_numero)
+# Variável para armazenar uma frase
+frase = "árvore é verde."
+
+# Imprime a frase armazenada na variável
+print("Frase armazenada:", frase)
+# Variável para armazenar uma frase
+frase = "árvore é verde."
+
+# Conta a quantidade de espaços em branco na frase
+quantidade_espacos = frase.count(' ')
+
+# Imprime a quantidade de espaços em branco na frase
+print("A quantidade de espaços em branco na frase é:", quantidade_espacos)
+# Variável para armazenar uma palavra
+palavra = "árvore"
+
+# Imprime a palavra armazenada na variável
+print("Palavra armazenada:", palavra)
+# Variável para armazenar uma palavra
+palavra = "árvore"
+
+# Conta a quantidade de letras na palavra
+quantidade_letras = len(palavra)
+
+# Imprime a quantidade de letras na palavra
+print("A quantidade de letras na palavra é:", quantidade_letras)
